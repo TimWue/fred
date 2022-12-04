3cdadbd3668dfd599d151a6c2a413ec0b1760d3f
Tim Wuellner <tim.wuellner@openknowledge.de>
2022-12-04 23:07:05 +0100

    started working with tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import React from "react";
-import "./App.css";
+import "./index.css";
 import { RegisterServer } from "./pages/RegisterServer";
 
 function App() {
   return (
-    <div>
-      <RegisterServer />
+    <div className="bg-gradient-to-r from-[#141E30] to-[#243B55] h-screen w-full flex justify-center">
+      {/*<RegisterServer />*/}
+      <div
+        className="w-1/4 h-1/2 bg-white bg-opacity-20 
+      backdrop-blur-lg rounded drop-shadow-lg mt-12 
+      border border-white min-w-fit flex flex-col items-center"
+      >
+        <h1 className={"text-2xl"}>Anmeldung</h1>
+        <div>
+          <input className={"w-80 h-8 ml-4 mr-4"}></input>
+          <input className={"w-80 h-8 ml-4 mr-4"}></input>
+        </div>
+      </div>
     </div>
   );
 }
